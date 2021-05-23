206c7b41e73c14d29d85c03c1da55b6c1fec101c
Adriano Reis <adriano_silvareis@hotmail.com>
2021-05-23 17:42:46 -0300

    feat(web): create auth-group service

--- packages/web/src/services/auth-group/auth-group-list.service.ts
+++ packages/web/src/services/auth-group/auth-group-list.service.ts
@@ -1,11 +1,10 @@
-// import { AuthGroup } from "@auth/entity";
-// import { client } from "@/config/http";
+import { ListAuthGroupRepository } from "@auth/use-case";
+import { AuthGroup } from "@auth/entity";
+import { client } from "@/config/http";
 
-// export default {
-//   async authGroupList(): Promise<AuthGroup[]> {
-//     const list = await client.get<AuthGroup[]>("/api/auth-groups");
-//     return list.data;
-//   },
-// };
-
-// export default class AuthGroupService implements AuthGroupRepository {}
+export class AuthGroupService implements ListAuthGroupRepository {
+  async list(): Promise<AuthGroup[]> {
+    const list = await client.get<AuthGroup[]>("/api/auth-groups");
+    return list.data;
+  }
+}
--- packages/web/src/store/views/auth-group/actions.ts
+++ packages/web/src/store/views/auth-group/actions.ts
@@ -1,10 +1,12 @@
 import { ActionTree } from "vuex";
 import { AuthGroupStore } from ".";
-import service from "@/services/auth-group/auth-group-list.service";
+import { AuthGroupService } from "@/services";
+
+const authGroup = new AuthGroupService();
 
 export default <ActionTree<AuthGroupStore, AuthGroupStore>>{
   async listAuthGroup(context) {
-    const list = await service.authGroupList();
+    const list = await authGroup.list();
     context.commit("setAuthGroupList", list);
   },
 };
