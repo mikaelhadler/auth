9148e54038918c57f960de522e5a13e0a7451c4a
Adriano Reis <adriano_silvareis@hotmail.com>
2021-06-02 19:25:22 -0300

    feat: remove activities of auth group list

--- packages/server/src/__tests__/adapters/gateways/auth-group/create-auth-group-repository.spec.ts
+++ packages/server/src/__tests__/adapters/gateways/auth-group/create-auth-group-repository.spec.ts
@@ -1,23 +1,23 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import { AuthGroupPrismaRepository } from "@/adapters/gateways/auth-group/auth-group-prisma-repository";
+import { AuthGroupPrismaRepository } from "@/adapters/gateways/auth-group/auth-group-prisma-repository"
 
-import { prismaMock } from "@/__tests__/frameworks/database/prisma-client-mock";
-import { CreateAuthGroupRepository } from "@auth/use-case";
-import { AuthGroupProperties } from "@auth/entity";
+import { prismaMock } from "@/__tests__/frameworks/database/prisma-client-mock"
+import { CreateAuthGroupRepository } from "@auth/use-case"
+import { AuthGroupProperties } from "@auth/entity"
 
 describe("CreateAuthGroupRepository", () => {
   it("should call create", async () => {
-    const { sut } = makeSut();
+    const { sut } = makeSut()
     const authGroup: AuthGroupProperties = {
       title: "Administrator",
       activities: [
         {
           id: "d0d8a425-7665-417d-9f70-0712c4bdae42",
           name: "auth-group",
-          permissions: ["readonly", "create", "delete", "update"],
-        },
-      ],
-    };
+          permissions: ["readonly", "create", "delete", "update"]
+        }
+      ]
+    }
 
     const expectedCall = {
       title: "Administrator",
@@ -25,27 +25,27 @@ describe("CreateAuthGroupRepository", () => {
         createMany: {
           data: [
             {
-              activity_id: "d0d8a425-7665-417d-9f70-0712c4bdae42",
-            },
-          ],
-        },
-      },
-    };
+              activity_id: "d0d8a425-7665-417d-9f70-0712c4bdae42"
+            }
+          ]
+        }
+      }
+    }
     // @ts-ignore
-    const spy = prismaMock.as_auth_groups.create;
-    await sut.create(authGroup);
+    const spy = prismaMock.as_auth_groups.create
+    await sut.create(authGroup)
 
     expect(spy).toHaveBeenCalledWith({
-      data: expectedCall,
-    });
-  });
-});
+      data: expectedCall
+    })
+  })
+})
 
 type SutTypes = {
-  sut: CreateAuthGroupRepository;
-};
+  sut: CreateAuthGroupRepository
+}
 
 function makeSut(): SutTypes {
-  const sut = new AuthGroupPrismaRepository();
-  return { sut };
+  const sut = new AuthGroupPrismaRepository()
+  return { sut }
 }
